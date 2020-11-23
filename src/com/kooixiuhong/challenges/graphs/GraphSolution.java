18a309b78df53862d8b15b8d10df5f2c801ddc15
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-11-23 18:59:55 +0000

    [graph] Add docstring for course schedule

--- src/com/kooixiuhong/challenges/graphs/GraphSolution.java
+++ src/com/kooixiuhong/challenges/graphs/GraphSolution.java
@@ -129,7 +129,22 @@ public class GraphSolution {
         return graph;
     }
 
-
+    /**
+     *
+     * There are a total of n courses you have to take labelled from 0 to n - 1.
+     *
+     * Some courses may have prerequisites, for example, if prerequisites[i] = [ai, bi]
+     * this means you must take the course bi before the course ai.
+     *
+     * Given the total number of courses numCourses and a list of the prerequisite pairs,
+     * return the ordering of courses you should take to finish all courses.
+     *
+     * If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+     *
+     * @param k
+     * @param prerequisites
+     * @return
+     */
     public int[] courseSchedule(int k, int[][] prerequisites) {
         List<Integer>[] adjList = new List[k];
         int[] indeg = new int[k];
